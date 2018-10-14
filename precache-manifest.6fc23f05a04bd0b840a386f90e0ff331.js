84d5335c1cec597e94683cc876e40124be680c5d
Jamie <hello@jamiebb.com>
2018-10-14 12:16:01 -0500

    Updates

---
+++ precache-manifest.6fc23f05a04bd0b840a386f90e0ff331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7b770323c873bf23713",
+    "url": "/portfolio_new/static/js/runtime~main.b7b77032.js"
+  },
+  {
+    "revision": "05923cff7f7c3e7b4231",
+    "url": "/portfolio_new/static/js/main.05923cff.chunk.js"
+  },
+  {
+    "revision": "302171cbdd9446ff32bd",
+    "url": "/portfolio_new/static/js/1.302171cb.chunk.js"
+  },
+  {
+    "revision": "05923cff7f7c3e7b4231",
+    "url": "/portfolio_new/static/css/main.7dd45b99.chunk.css"
+  },
+  {
+    "revision": "7ca1c8d4c99887fc256f2955de2cc21e",
+    "url": "/portfolio_new/index.html"
+  }
+];
