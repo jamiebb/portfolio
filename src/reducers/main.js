e53b1b3badba734c3d8f90cda4085c36964f1920
Jamie <jamieandburton@gmail.com>
2020-06-13 14:40:39 -0500

    updating portfolio languages

--- src/reducers/main.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux'
-import { projects } from './projects'
-import { skills } from './skills'
-
-export const AppStore = combineReducers({ projects, skills });
--- src/reducers/skills.js
+++
@@ -1,9 +0,0 @@
-export const skills = (state = 
-    ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Vue', 'jQuery', 'Node', 'MongoDB',
-    'Sass', 'Extensions', 'Illustrator', 'Photoshop'],
-     action) => {
-    switch (action.type) {
-        default:
-            return state;
-    }
-}
