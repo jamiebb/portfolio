2df3ba99c5d92eaf1e726825e846ef54bdfe9929
Jamie <jamieandburton@gmail.com>
2020-08-02 11:31:09 -0500

    moving files

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -25,7 +24,4 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin()
-  ],
 };
