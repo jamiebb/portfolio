d080b81f75c3646faca61b772020c47a249a5489
Jamie <jamieandburton@gmail.com>
2020-11-29 14:43:00 -0600

    updating skills, resume

--- webpack.common.js
+++ webpack.common.js
@@ -13,7 +13,7 @@ module.exports = {
         use: ['html-loader'],
       },
       {
-        test: /\.(svg|png|jpg|gif)$/,
+        test: /\.(png|jpg)$/,
         use: {
           loader: 'file-loader',
           options: {
